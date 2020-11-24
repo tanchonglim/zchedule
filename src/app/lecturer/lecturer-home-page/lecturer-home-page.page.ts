40eb1c7e0c9c7e3d6d77233eba2400637051904f
xiangyeoh <67411527+xiangyeoh@users.noreply.github.com>
2020-11-24 09:54:25 +0800

    lect search

--- src/app/lecturer/lecturer-home-page/lecturer-home-page.page.ts
+++ src/app/lecturer/lecturer-home-page/lecturer-home-page.page.ts
@@ -3,13 +3,33 @@ import { ModalController } from "@ionic/angular";
 import { LecturerTimetableComponent } from "../components/lecturer-timetable/lecturer-timetable.component";
 import { TeachingSubjectComponent } from "../components/teaching-subject/teaching-subject.component";
 
+interface lecturerList {
+  lectName: string;
+  lectId: string;
+  lectCourse: string;
+}
+
 @Component({
   selector: "app-lecturer-home-page",
   templateUrl: "./lecturer-home-page.page.html",
   styleUrls: ["./lecturer-home-page.page.scss"],
 })
 export class LecturerHomePagePage implements OnInit {
-  id: string = "13914";
+  id: string;
+  course: string;
+  lecturerList: Array<lecturerList> = [
+    {
+      lectName: "ali",
+      lectId: "abc",
+      lectCourse: "SCSJ",
+    },
+    {
+      lectName: "abu",
+      lectId: "abqweqwc",
+      lectCourse: "SCSV",
+    },
+  ];
+  isFound: any;
   constructor(public modal: ModalController) {}
 
   ngOnInit() {}
@@ -33,5 +53,21 @@ export class LecturerHomePagePage implements OnInit {
     });
     await modal.present();
     await modal.onWillDismiss();
+  }
+
+  // checkLect(){
+  //   return this.lecturerList.lectName = this.id;
+  // }
+
+  searchLect() {
+    console.log(this.id);
+    this.isFound = this.lecturerList.find((lect) => {
+      return lect.lectName == this.id && lect.lectCourse == this.course;
+    });
+    console.log(this.isFound);
+  }
+
+  getCourse() {
+    console.log(this.course);
   }
 }

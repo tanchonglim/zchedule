0929359162bb12e1e556bea084589cd0d8664d3a
tanchonglim <tanchonglim98@gmail.com>
2021-01-04 13:16:50 +0800

    login design

--- src/app/user/Theme.ts
+++ src/app/user/Theme.ts
@@ -2,15 +2,16 @@ import { IonicColors } from "ionic-angular-theme-switch";
 
 export let themes: Array<IonicColors> = [
   {
-    primary: "blue",
-    secondary: "#5fb3b3",
-    tertiary: "#fac863",
+    primary: "rgba(150,53,144,1)",
+    secondary: "rgba(59,0,87,1)",
+    tertiary: "rgba(239,169,175,1)",
     success: "#90d089",
-    warning: "#f99157",
+    warning: "rgba(239,237,169,1)",
     danger: "#ec5f67",
+    light: "white",
   },
   {
-    primary: "purple",
+    primary: "blue",
     secondary: "#5fb3b3",
     tertiary: "#fac863",
     success: "#90d089",
